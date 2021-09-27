319db455df2ea1256a7615ad6c0c031ea00f4774
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2021-09-27 19:45:14 +0300

    Homework solved

--- Module1/Homework/src/hw3/ChooseStudent.java
+++ Module1/Homework/src/hw3/ChooseStudent.java
@@ -1,6 +1,7 @@
 package hw3;
 
-import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
@@ -13,6 +14,10 @@ public class ChooseStudent {
     public static void main(String[] args) {
         var students = getStudents();
 
+        if(students == null){
+            System.exit(0);
+        }
+
         Scanner scanner = new Scanner(System.in);
 
         String input;
@@ -23,11 +28,11 @@ public class ChooseStudent {
                 case "/list" -> listAllStudents(students);
                 case "/q" -> listAllStudentsExit(students);
             }
-        }while (input != "/q");
+        }while (!input.equals("/q"));
 
     }
 
-    private static void listAllStudentsExit(Student[] students) {
+    private static void listAllStudentsExit(ArrayList<Student> students) {
         for (var student :
                 students) {
             System.out.println(student.toStringWithGrade());
@@ -36,51 +41,79 @@ public class ChooseStudent {
         System.exit(0);
     }
 
-    private static void chooseRandomStudent(Student[] students, Scanner scanner) {
+    /**
+     * Gets a random student (if available and asks question about them.)
+     * @param students All students initialized by the program.
+     * @param scanner A scanner object for reading user input.
+     */
+    private static void chooseRandomStudent(ArrayList<Student> students, Scanner scanner) {
         Random random = new Random();
 
-        var randomNumber = random.nextInt(0, students.length);
+        // Check if there are available students.
+        var availableStudents = students.stream().filter(s -> !s.isChecked).toList();
+        if(availableStudents.size() == 0){
+            System.out.println("All students have been checked.");
+            return;
+        }
 
-        var chosenStudent = students[randomNumber];
+        var randomNumber = random.nextInt(0, availableStudents.size());
+        var chosenStudent = availableStudents.get(randomNumber);
 
-        if(!chosenStudent.isPresent){
-            var answer = "";
+        chosenStudent.isChecked = true;
 
-            while (answer != "Y" && answer != "N") {
-                System.out.println(chosenStudent + " was randomly selected.");
-                System.out.print("Is student present? (Y/N):");
-                answer = scanner.nextLine();
+        var answer = "";
 
-                if(Objects.equals(answer, "Y")){
-                    chosenStudent.isPresent = true;
-                    int grade = 0;
-                    while (grade <1 || grade > 10){
-                        System.out.print("Enter grade for " + chosenStudent.name + ' ' + chosenStudent.surname +
-                                " (1 - 10) : ");
-                        grade = scanner.nextInt();
-                    }
-                    chosenStudent.grade = grade;
-                    return;
+        //Ask questions
+        while (!(Objects.equals(answer, "Y") || Objects.equals(answer, "N"))) {
+            System.out.println(chosenStudent + " was randomly selected.");
+            System.out.print("Is student present? (Y/N):");
+            answer = scanner.nextLine();
+
+            if(Objects.equals(answer, "Y")){
+                chosenStudent.isPresent = true;
+                int grade = 0;
+                while (grade <1 || grade > 10){
+                    System.out.print("Enter grade for " + chosenStudent.name + ' ' + chosenStudent.surname +
+                            " (1 - 10) : ");
+                    grade = scanner.nextInt();
                 }
-                if(Objects.equals(answer, "N")){
-                    chooseRandomStudent(students, scanner);
-                    return;
-                }
+                chosenStudent.grade = grade;
+                return;
+            }
+            if(Objects.equals(answer, "N")){
+                chooseRandomStudent(students, scanner);
+                return;
             }
         }
+
     }
 
-    private static void listAllStudents(Student[] students) {
+    private static void listAllStudents(ArrayList<Student> students) {
         for (var student :
                 students) {
             System.out.println(student);
         }
     }
 
-    static Student[] getStudents(){
-        return new Student[]{
-                new Student("Pavel", "Dzhalev"),
-                new Student("Ivan", "Marev")
-        };
+    static ArrayList<Student> getStudents(){
+        var students = new ArrayList<Student>();
+        try {
+            var file = new File("resources/students.txt");
+            var fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()){
+                var studentString = fileReader.nextLine();
+                try {
+                    students.add(Student.parseStudent(studentString));
+                }catch (Exception e){
+                    System.out.println(e.getMessage());
+                    return null;
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return students;
     }
 }
--- Module1/Homework/src/hw3/Student.java
+++ Module1/Homework/src/hw3/Student.java
@@ -5,10 +5,19 @@ public class Student {
     public String surname;
     public int grade;
     public boolean isPresent;
+    public boolean isChecked;
 
     public Student(String name, String surname) {
         this.name = name;
         this.surname = surname;
+    }
+
+    public static Student parseStudent(String input){
+        var data = input.split(" ");
+        if(data.length > 2){
+            throw new IllegalArgumentException("Error parsing students. Please check text file.");
+        }
+        return new Student(data[0], data[1]);
     }
 
     @Override
@@ -17,6 +26,7 @@ public class Student {
     }
 
     public String toStringWithGrade(){
-        return name +' '+ surname +' '+ grade;
+        var presentString = isPresent?"present":"not present";
+        return name + ' ' + surname +" ("+ presentString +") " + grade;
     }
 }
