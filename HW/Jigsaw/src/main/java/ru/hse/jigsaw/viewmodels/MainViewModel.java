03ce8b20726ef39614c636dd5bf52ea7f3b319a5
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2022-05-13 11:56:03 +0300

    Add timer
    Added the application timer to the user interface so that the player sees how much time they have spent playing the game in the current session.

--- HW/Jigsaw/src/main/java/ru/hse/jigsaw/viewmodels/MainViewModel.java
+++ HW/Jigsaw/src/main/java/ru/hse/jigsaw/viewmodels/MainViewModel.java
@@ -1,17 +1,20 @@
 package ru.hse.jigsaw.viewmodels;
-import de.saxsys.mvvmfx.*;
-import javafx.beans.property.Property;
+
+import de.saxsys.mvvmfx.ViewModel;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
-import ru.hse.jigsaw.models.Tile;
-import ru.hse.jigsaw.views.MainView;
 
 public class MainViewModel implements ViewModel{
     @FXML
     GridPane gridPane;
 
-    private SimpleIntegerProperty turns = new SimpleIntegerProperty(0);
+    private final SimpleIntegerProperty turns = new SimpleIntegerProperty(0);
+    private final SimpleIntegerProperty elapsedTime = new SimpleIntegerProperty(0);
+    private long start;
+
+    public Task stopwatch;
 
     public Integer getTurns() {
         return turns.getValue();
@@ -21,10 +24,38 @@ public class MainViewModel implements ViewModel{
         return turns;
     }
 
+    public SimpleIntegerProperty elapsedTimeProperty() {
+        return elapsedTime;
+    }
+
+    public void startTimer(){
+        start = System.currentTimeMillis();
+
+        new Thread(stopwatch).start();
+    }
+
     public void setTurns(Integer turns) {
         this.turns.setValue(turns);
     }
 
     public MainViewModel(){
+        stopwatch = new Task() {
+            @Override
+            protected Object call() throws Exception {
+                while(true){
+
+
+                    var current = System.currentTimeMillis();
+
+                    updateValue((current - start)/1000);
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
     }
 }
--- HW/Jigsaw/src/main/java/ru/hse/jigsaw/views/MainView.java
+++ HW/Jigsaw/src/main/java/ru/hse/jigsaw/views/MainView.java
@@ -7,12 +7,15 @@ import javafx.beans.property.Property;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import javafx.util.converter.NumberStringConverter;
 import ru.hse.jigsaw.viewmodels.MainViewModel;
 
@@ -28,7 +31,11 @@ public class MainView implements FxmlView<MainViewModel>, Initializable {
     @FXML
     GridPane piece;
     @FXML
+    Group piece2;
+    @FXML
     Label turnsLabel;
+    @FXML
+    Label time;
 
     @InjectViewModel
     private MainViewModel mainViewModel;
@@ -36,7 +43,21 @@ public class MainView implements FxmlView<MainViewModel>, Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         buildBoard();
-        makeDraggable(piece);
+        makeDraggable(piece2);
+        mainViewModel.startTimer();
+
+        turnsLabel.textProperty().bind(mainViewModel.turnsProperty().asString());
+
+        //time.textProperty().bind(mainViewModel.elapsedTimeProperty().asString());
+
+        time.textProperty().bind(mainViewModel.stopwatch.valueProperty().asString());
+
+        board.setOnDragDropped(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent dragEvent) {
+                System.out.println("Draggg");
+            }
+        });
     }
 
     private double startX;
@@ -71,29 +92,59 @@ public class MainView implements FxmlView<MainViewModel>, Initializable {
     }
 
     private void clearPieceGenerator(){
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                var rect = new Rectangle(CELL_SIZE, CELL_SIZE);
-                rect.setStyle("-fx-fill: white; -fx-stroke: black; -fx-stroke-width: 1;");
-                piece.add(rect, i, j);
-            }
-        }
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 3; j++) {
+//                var rect = new Rectangle(CELL_SIZE, CELL_SIZE);
+//                rect.setStyle("-fx-fill: white; -fx-stroke: black; -fx-stroke-width: 1;");
+//                piece.add(rect, i, j);
+//            }
+//        }
     }
 
     @FXML
     private void generatePiece(){
         var rand = new Random();
         clearPieceGenerator();
-        
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if(rand.nextInt(0, 2) % 2 == 0){
-                    var rect = new Rectangle(CELL_SIZE, CELL_SIZE);
-                    rect.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
-                    piece.add(rect, i, j);
-                }
-            }
+
+        // Decide which piece to use. There are 9 possible figures
+        int pieceVariation = rand.nextInt(9); // possible results are [0, 8] - 9 options
+
+        piece2.getChildren().clear();
+        switch (pieceVariation){
+            default:
+                // Add first rectangle to group
+                var rect1 = new Rectangle(CELL_SIZE, CELL_SIZE);
+                rect1.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+
+                // Add second rectangle to group
+                var rect2 = new Rectangle(CELL_SIZE, CELL_SIZE);
+                rect2.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+                rect2.setX(CELL_SIZE);
+
+                var rect3 = new Rectangle(CELL_SIZE, CELL_SIZE);
+                rect3.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+                rect3.setY(CELL_SIZE);
+
+                var rect4 = new Rectangle(CELL_SIZE, CELL_SIZE);
+                rect4.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+                rect4.setY(CELL_SIZE*2);
+
+                // Add all pieces to the group
+                piece2.getChildren().addAll(rect1, rect2, rect3, rect4);
+
+                var rotationRand = rand.nextInt(0, 3);
+                piece2.setRotate(90*rotationRand);
         }
+        
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 3; j++) {
+//                if(rand.nextInt(0, 2) % 2 == 0){
+//                    var rect = new Rectangle(CELL_SIZE, CELL_SIZE);
+//                    rect.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+//                    piece.add(rect, i, j);
+//                }
+//            }
+//        }
 
         mainViewModel.setTurns(mainViewModel.getTurns() + 1);
     }
