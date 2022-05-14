9afae5013348116b5dfab0e1ed5b1fbd75487c8a
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2022-05-14 11:06:46 +0300

    Add new figures
    Added support for more figures to be generated.

--- HW/Jigsaw/src/main/java/ru/hse/jigsaw/views/MainView.java
+++ HW/Jigsaw/src/main/java/ru/hse/jigsaw/views/MainView.java
@@ -2,21 +2,17 @@ package ru.hse.jigsaw.views;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.Property;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
-import javafx.util.converter.NumberStringConverter;
 import ru.hse.jigsaw.viewmodels.MainViewModel;
 
 import java.net.URL;
@@ -48,14 +44,42 @@ public class MainView implements FxmlView<MainViewModel>, Initializable {
 
         turnsLabel.textProperty().bind(mainViewModel.turnsProperty().asString());
 
+        board.setOnMouseClicked(clicked -> System.out.println("board clicked!"));
+
         //time.textProperty().bind(mainViewModel.elapsedTimeProperty().asString());
+
+        piece2.setOnDragDetected(dragged ->
+        {
+            Dragboard db = piece2.startDragAndDrop(TransferMode.NONE);
+
+            ClipboardContent content = new ClipboardContent();
+            content.putString("qj mi");
+            db.setContent(content);
+        });
 
         time.textProperty().bind(mainViewModel.stopwatch.valueProperty().asString());
 
-        board.setOnDragDropped(new EventHandler<DragEvent>() {
+        board.setOnDragDropped(dropped -> {
+                System.out.println("Draggg");
+            }
+        );
+
+
+//        piece2.setOnDragDetected(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//                System.out.println("Grafgggggg");
+//
+//                mouseEvent.consume();
+//            }
+//        });
+
+        turnsLabel.setOnDragDetected(dragDetected -> System.out.println("heyy"));
+
+        board.setOnDragOver(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent dragEvent) {
-                System.out.println("Draggg");
+                System.out.println("kura mi qnko!");
             }
         });
     }
@@ -87,18 +111,8 @@ public class MainView implements FxmlView<MainViewModel>, Initializable {
         clearPieceGenerator();
     }
 
-    private void onGridDragEnter(){
-        System.out.println("Draggg");
-    }
-
     private void clearPieceGenerator(){
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                var rect = new Rectangle(CELL_SIZE, CELL_SIZE);
-//                rect.setStyle("-fx-fill: white; -fx-stroke: black; -fx-stroke-width: 1;");
-//                piece.add(rect, i, j);
-//            }
-//        }
+        // TODO: Implement clear of new piece
     }
 
     @FXML
@@ -107,44 +121,80 @@ public class MainView implements FxmlView<MainViewModel>, Initializable {
         clearPieceGenerator();
 
         // Decide which piece to use. There are 9 possible figures
-        int pieceVariation = rand.nextInt(9); // possible results are [0, 8] - 9 options
+        int pieceVariation = rand.nextInt(5); // possible results are [0, 8] - 9 options
 
         piece2.getChildren().clear();
-        switch (pieceVariation){
-            default:
-                // Add first rectangle to group
-                var rect1 = new Rectangle(CELL_SIZE, CELL_SIZE);
-                rect1.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
 
-                // Add second rectangle to group
-                var rect2 = new Rectangle(CELL_SIZE, CELL_SIZE);
-                rect2.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+        var rect1 = new Rectangle(CELL_SIZE, CELL_SIZE);
+        rect1.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+
+        var rect2 = new Rectangle(CELL_SIZE, CELL_SIZE);
+        rect2.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+
+        var rect3 = new Rectangle(CELL_SIZE, CELL_SIZE);
+        rect3.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+
+        var rect4 = new Rectangle(CELL_SIZE, CELL_SIZE);
+        rect4.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+
+        var rect5 = new Rectangle(CELL_SIZE, CELL_SIZE);
+        rect4.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
+
+        // Generating different shapes here
+        switch (4){
+            case 0:
                 rect2.setX(CELL_SIZE);
-
-                var rect3 = new Rectangle(CELL_SIZE, CELL_SIZE);
-                rect3.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
                 rect3.setY(CELL_SIZE);
-
-                var rect4 = new Rectangle(CELL_SIZE, CELL_SIZE);
-                rect4.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
                 rect4.setY(CELL_SIZE*2);
-
-                // Add all pieces to the group
                 piece2.getChildren().addAll(rect1, rect2, rect3, rect4);
+                break;
+            case 1:
+                rect2.setY(CELL_SIZE);
+                rect3.setY(CELL_SIZE);
+                rect3.setX(CELL_SIZE);
+                rect4.setX(CELL_SIZE);
+                rect4.setY(CELL_SIZE*2);
+                piece2.getChildren().addAll(rect1, rect2, rect3, rect4);
+                break;
+            case 3:
+                rect2.setX(CELL_SIZE);
+                rect3.setX(CELL_SIZE*2);
+                rect4.setY(CELL_SIZE);
+                rect5.setY(CELL_SIZE*2);
+                piece2.getChildren().addAll(rect1, rect2, rect3, rect4, rect5);
+                break;
+            case 4:
+                rect1.setX(CELL_SIZE);
+                rect2.setX(CELL_SIZE);
+                rect2.setY(CELL_SIZE);
+                rect3.setX(CELL_SIZE);
+                rect3.setY(CELL_SIZE*2);
+                rect4.setY(CELL_SIZE*2);
+                rect5.setY(CELL_SIZE*2);
+                rect5.setX(CELL_SIZE*2);
+                piece2.getChildren().addAll(rect1, rect2, rect3, rect4, rect5);
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            default:
 
-                var rotationRand = rand.nextInt(0, 3);
-                piece2.setRotate(90*rotationRand);
+                break;
         }
-        
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                if(rand.nextInt(0, 2) % 2 == 0){
-//                    var rect = new Rectangle(CELL_SIZE, CELL_SIZE);
-//                    rect.setStyle("-fx-fill: blue; -fx-stroke: black; -fx-stroke-width: 1;");
-//                    piece.add(rect, i, j);
-//                }
-//            }
-//        }
+
+        // Add all pieces to the group
+
+
+        var rotationRand = rand.nextInt(0, 3);
+        piece2.setRotate(90*rotationRand);
+
+        var flipRand = rand.nextInt(0, 2);
+        var flip = new Rotate(180*flipRand, CELL_SIZE*1.5, CELL_SIZE*1.5, 0.0, new Point3D(0.0, 1.0, 0.0));
+
+        piece2.getTransforms().add(flip);
 
         mainViewModel.setTurns(mainViewModel.getTurns() + 1);
     }
